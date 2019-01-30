6ac058cd6d18b7b46f37752666aa1e74436651f8
James Li <dawwctor@stanford.edu>
2019-01-29 17:57:21 -0800

    Added getters/setters to Event and Pin classes + preliminary mocks of getEvents and getFood.

--- app/src/main/java/stanford/cs194/stanfood/Event.java
+++ app/src/main/java/stanford/cs194/stanfood/Event.java
@@ -1,4 +1,57 @@
 package stanford.cs194.stanfood;
 
+import java.time.LocalDateTime;
+
 public class Event {
+    private String eventId;
+    private String description;
+    private String locationName;
+    private LocalDateTime time;
+
+    public Event() {}
+
+    public Event(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    /*
+     * Gets the food of the current event
+     */
+    public Food getFood(String eventId) {
+        Food food = new Food();
+        // Lookup food from database using eventId
+        return food;
+    }
 }
