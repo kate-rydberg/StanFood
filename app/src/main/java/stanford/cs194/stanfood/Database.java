89443bedf2c622eef2ac766a0aae7140cac65c12
Tofe Alimi <oalimi@stanford.edu>
2019-01-29 19:24:15 -0800

    [lux] implemented user interfacing

--- app/src/main/java/stanford/cs194/stanfood/Database.java
+++ app/src/main/java/stanford/cs194/stanfood/Database.java
@@ -5,11 +5,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Database {
-    final private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference dbRef;
     final private String dbPath = "https://stanfood-e7255.firebaseio.com/";
+    private FirebaseDatabase database;
+    private DatabaseReference dbRef;
+
     public Database(){
-        dbRef = database.getReference(dbPath);
+        database = FirebaseDatabase.getInstance(dbPath);
+        dbRef = database.getReference();
     }
     public String createUser(String email, String name){
         DatabaseReference userRef = dbRef.child("users");
@@ -20,7 +22,7 @@ public class Database {
     public Object createPin(LatLng coordinate){
         return null;
     }
-    public Object createEvent(int pin_id, String description, String locationName){
+    public Object createEvent(String description, String locationName){
         return null;
     }
     public Object createFood(int event_id, String description){
