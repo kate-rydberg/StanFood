7c62a4207116c5188473e55f12e16010692e6672
cindydj <cindyj@stanford.edu>
2019-02-10 17:16:02 -0800

    add splash screen with stanfood logo

---
+++ app/src/main/java/stanford/cs194/stanfood/SplashActivity.java
@@ -0,0 +1,20 @@
+package stanford.cs194.stanfood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ssaurel on 02/12/2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
