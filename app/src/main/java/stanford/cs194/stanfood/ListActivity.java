716ec256e656adeebfe58964b7c6820ff19add5c
Rachel <rhyon@stanford.edu>
2019-02-11 10:10:26 -0800

    change start time to type long

--- app/src/main/java/stanford/cs194/stanfood/ListActivity.java
+++ app/src/main/java/stanford/cs194/stanfood/ListActivity.java
@@ -23,6 +23,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Attr;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
@@ -31,125 +33,14 @@ import java.util.HashMap;
 public class ListActivity extends ListView {
     private Database db;
     private ArrayList<String> name;
-    private ArrayList<LocalDateTime> time;
+    private ArrayList<Long> time;
     private ArrayList<String> description;
     private Context context;
     private LatLng markerlocation;
     private HashMap<LatLng,String> eventStorage;
 
-    public ListActivity(Context context, Database db, Marker marker, HashMap<LatLng, String> eventStorage) {
-
-        super(context, new AttributeSet() {
-            @Override
-            public int getAttributeCount() {
-                return 0;
-            }
-
-            @Override
-            public String getAttributeName(int index) {
-                return null;
-            }
-
-            @Override
-            public String getAttributeValue(int index) {
-                return null;
-            }
-
-            @Override
-            public String getAttributeValue(String namespace, String name) {
-                return null;
-            }
-
-            @Override
-            public String getPositionDescription() {
-                return null;
-            }
-
-            @Override
-            public int getAttributeNameResource(int index) {
-                return 0;
-            }
-
-            @Override
-            public int getAttributeListValue(String namespace, String attribute, String[] options, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public boolean getAttributeBooleanValue(String namespace, String attribute, boolean defaultValue) {
-                return false;
-            }
-
-            @Override
-            public int getAttributeResourceValue(String namespace, String attribute, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public int getAttributeIntValue(String namespace, String attribute, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public int getAttributeUnsignedIntValue(String namespace, String attribute, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public float getAttributeFloatValue(String namespace, String attribute, float defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public int getAttributeListValue(int index, String[] options, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public boolean getAttributeBooleanValue(int index, boolean defaultValue) {
-                return false;
-            }
-
-            @Override
-            public int getAttributeResourceValue(int index, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public int getAttributeIntValue(int index, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public int getAttributeUnsignedIntValue(int index, int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public float getAttributeFloatValue(int index, float defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public String getIdAttribute() {
-                return null;
-            }
-
-            @Override
-            public String getClassAttribute() {
-                return null;
-            }
-
-            @Override
-            public int getIdAttributeResourceValue(int defaultValue) {
-                return 0;
-            }
-
-            @Override
-            public int getStyleAttribute() {
-                return 0;
-            }
-        });
+    public ListActivity(Context context, AttributeSet attr, Database db, Marker marker, HashMap<LatLng, String> eventStorage) {
+        super(context,attr);
         this.context = context;
         this.db = db;
         this.markerlocation = marker.getPosition();
@@ -173,7 +64,7 @@ public class ListActivity extends ListView {
                             Event event = ds.getValue(Event.class);
                             if(event.getPinId() == eventStorage.get(markerlocation)){
                                 description.add(event.getDescription());
-                                time.add(event.getTime());
+                                time.add(event.getTimeStart());
                                 name.add(event.getEventId());
                             }
                         }
@@ -199,7 +90,7 @@ public class ListActivity extends ListView {
 
         //this code sets the values of the objects to values from the arraylists
         eventName.setText(name.get(position));
-        eventTimeStart.setText(time.get(position).toString());
+        eventTimeStart.setText(Long.toString(time.get(position)));
         eventDescription.setText(description.get(position));
 
         return rowView;
--- app/src/main/java/stanford/cs194/stanfood/MapsActivity.java
+++ app/src/main/java/stanford/cs194/stanfood/MapsActivity.java
@@ -151,10 +151,120 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         bottomSheet.expand();
         mMap.setPadding(0, 0, 0, (int)getResources().getDimension(R.dimen.bottom_sheet_expanded_height));
         mMap.animateCamera(CameraUpdateFactory.newLatLng(location),500,null);
+        /*AttributeSet attr = new AttributeSet() {
+            @Override
+            public int getAttributeCount() {
+                return 0;
+            }
 
-        //ListView listview = findViewById(R.id.listViewBtmSheet);
+            @Override
+            public String getAttributeName(int index) {
+                return null;
+            }
+
+            @Override
+            public String getAttributeValue(int index) {
+                return null;
+            }
+
+            @Override
+            public String getAttributeValue(String namespace, String name) {
+                return null;
+            }
+
+            @Override
+            public String getPositionDescription() {
+                return null;
+            }
+
+            @Override
+            public int getAttributeNameResource(int index) {
+                return 0;
+            }
+
+            @Override
+            public int getAttributeListValue(String namespace, String attribute, String[] options, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public boolean getAttributeBooleanValue(String namespace, String attribute, boolean defaultValue) {
+                return false;
+            }
+
+            @Override
+            public int getAttributeResourceValue(String namespace, String attribute, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public int getAttributeIntValue(String namespace, String attribute, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public int getAttributeUnsignedIntValue(String namespace, String attribute, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public float getAttributeFloatValue(String namespace, String attribute, float defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public int getAttributeListValue(int index, String[] options, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public boolean getAttributeBooleanValue(int index, boolean defaultValue) {
+                return false;
+            }
+
+            @Override
+            public int getAttributeResourceValue(int index, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public int getAttributeIntValue(int index, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public int getAttributeUnsignedIntValue(int index, int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public float getAttributeFloatValue(int index, float defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public String getIdAttribute() {
+                return null;
+            }
+
+            @Override
+            public String getClassAttribute() {
+                return null;
+            }
+
+            @Override
+            public int getIdAttributeResourceValue(int defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public int getStyleAttribute() {
+                return 0;
+            }
+        };
+        ListView listview = findViewById(R.id.listViewBtmSheet);
         //Xml.asAttributeSet()
-        //eventList = new ListActivity(listview.getContext(), db, marker,eventStorage);
+        eventList = new ListActivity(listview.getContext(), attr, db, marker,eventStorage);*/
 
         return true;
     }
