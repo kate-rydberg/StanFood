748c2ec1a63b7b26456715d3b7f7bf0b92601f4f
Po Tsui <potsui@stanford.edu>
2019-02-10 16:47:04 -0800

    Open up MapsActivity on notification tap

--- app/src/main/java/stanford/cs194/stanfood/Notification.java
+++ app/src/main/java/stanford/cs194/stanfood/Notification.java
@@ -2,7 +2,9 @@ package stanford.cs194.stanfood;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -43,12 +45,24 @@ public class Notification {
     public void sendNotification(String title, String content, int notificationId) {
         Log.d("Notification", "Sending notification with title: " + title
                 + ", content: " + content + ", notificationId: " + notificationId);
+
+        /*
+         * TODO We will need to pass in additional information to this intent and
+         * TODO MapsActivity in order to automatically center the map and pull up event details
+         * TODO when the user clicks on the notification
+         */
+        Intent intent = new Intent(context, MapsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(title)
                 .setContentText(content)
                 .setTimeoutAfter(DEFAULT_NOTIFICATION_TIMEOUT_MS)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
 
         // notificationId is a unique int for each notification that you must define
         notificationManagerCompat.notify(notificationId, mBuilder.build());
