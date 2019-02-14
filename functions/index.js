cbca9b0c73e584d30fa203941fdfa1f8a00bdd2a
Tofe Alimi <oalimi@stanford.edu>
2019-02-14 01:06:19 -0800

    [ta] updated MapsActivity and added Google Cloud function to handle event expiration

---
+++ functions/index.js
@@ -0,0 +1,47 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.checkPinEvents = functions.https.onRequest((req, res) => {
+
+  //create database refs
+  var pinsRef = admin.database().ref('/pins');
+  var eventsRef = admin.database().ref('/events');
+
+  eventsRef.once('value', (snapshot) => {
+  	snapshot.forEach((childSnapshot) => {
+  		var data = childSnapshot.val();
+  		try {
+  			var timeStart = data.timeStart;
+  			var duration = data.duration;
+  			var curTime = new Date().getTime();
+
+  			// if event is expired
+  			if(timeStart + duration < curTime){
+  				var eventKey = childSnapshot.key;
+  				var pinId = data.pinId;
+
+  				// remove expired event, decrement numEvents on associated pin
+  				pinsRef.child(pinId).child('numEvents').transaction((numEvents) => {
+  					eventsRef.child(eventKey).remove();
+  					if(numEvents) {
+  						numEvents--;
+  						console.log('Expired event ' + eventKey + ' for pin ' + pinId +
+  						'. This pin now has ' + numEvents + ' event(s).');
+  					}
+  					return numEvents;
+  				});
+  			}
+  		}
+  		catch(err) {
+  			console.log(err);
+  			res.redirect(404);
+  		}
+  	})
+  })
+
+  //send back response
+  res.redirect(200);
+
+});
