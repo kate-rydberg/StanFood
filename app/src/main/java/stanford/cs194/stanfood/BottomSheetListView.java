0805d2a8779d8f8dde8c7a59eed2bd9848640847
Rachel <rhyon@stanford.edu>
2019-02-17 17:01:21 -0800

    event scrolling fixed

---
+++ app/src/main/java/stanford/cs194/stanfood/BottomSheetListView.java
@@ -0,0 +1,57 @@
+package stanford.cs194.stanfood;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.NestedScrollView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+public class BottomSheetListView extends ListView {
+
+    public BottomSheetListView (Context context, AttributeSet p_attrs) {
+        super (context, p_attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (canScrollVertically(this)) {
+            NestedScrollView parent = (NestedScrollView) getParent();
+            ViewCompat.setNestedScrollingEnabled(parent, false);
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (canScrollVertically(this)) {
+            NestedScrollView parent = (NestedScrollView) getParent();
+            ViewCompat.setNestedScrollingEnabled(parent, false);
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    /**
+     * Checks for whether the listview is at top or if all of the items
+     * are available in the view.
+     */
+    /
+    public boolean canScrollVertically (AbsListView view) {
+        boolean canScroll = false;
+
+        if (view != null && view.getChildCount() > 0) {
+
+            boolean isOnTop = view.getFirstVisiblePosition() != 0 || view.getChildAt(0).getTop() != 0;
+            boolean isAllItemsVisible = isOnTop && getLastVisiblePosition() == view.getChildCount();
+
+            if (isOnTop || isAllItemsVisible)  canScroll = true;
+        }
+
+        return canScroll;
+    }
+
+}
--- app/src/main/java/stanford/cs194/stanfood/adapters/EventAdapter.java
+++ app/src/main/java/stanford/cs194/stanfood/adapters/EventAdapter.java
@@ -2,6 +2,7 @@ package stanford.cs194.stanfood.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
