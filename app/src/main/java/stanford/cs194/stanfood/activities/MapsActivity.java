3466cc0b4600aec46bfec0f2b97e4e8c20dc6cd9
Rachel <rhyon@stanford.edu>
2019-03-03 13:29:26 -0800

    got rid of imports that are not needed

--- app/src/main/java/stanford/cs194/stanfood/activities/MapsActivity.java
+++ app/src/main/java/stanford/cs194/stanfood/activities/MapsActivity.java
@@ -9,7 +9,6 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
