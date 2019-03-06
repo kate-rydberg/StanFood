45c01ea8cf491c3064aef67e973f44f024739c2f
Rachel <rhyon@stanford.edu>
2019-03-05 17:34:25 -0800

    styling the map

--- app/src/main/java/stanford/cs194/stanfood/activities/LoginActivity.java
+++ app/src/main/java/stanford/cs194/stanfood/activities/LoginActivity.java
@@ -40,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+                        .setLogo(R.drawable.ic_launcher)
                         .build(),
                 RC_SIGN_IN);
     }
