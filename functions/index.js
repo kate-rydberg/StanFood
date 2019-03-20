df120dff28c8fc7a9f37399bb565f674cd511cd0
Po Tsui <potsui@stanford.edu>
2019-03-19 22:03:00 -0700

    Require dependency on momentjs

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
-const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const moment = require('moment');
+
 admin.initializeApp(functions.config().firebase);
 
 exports.checkPinEvents = functions.https.onRequest((req, res) => {
